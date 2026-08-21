91dba785348be9b078a9d26f5d1639fbd3fb9f5e
nuttapol-sup <nattapon.nook@gmail.com>
2026-08-21 09:53:51 +0700

    Set domain name to telecom.tmd.go.th in robots.txt and sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/api/",
     },
-    sitemap: "https://gts.tmd.go.th/sitemap.xml",
+    sitemap: "https://telecom.tmd.go.th/sitemap.xml",
   };
 }
